0d439e692de6a3fc6f46a9fb11df6750f8d513d1
Marcin Wierzbiński <marcin119a@gmail.com>
2026-02-13 11:36:01 +0100

    progress:

--- k8s-start/deploy.sh
+++ k8s-start/deploy.sh
@@ -1,7 +1,8 @@
 #minikube start
 
+kubectl apply -f api-configmap.yaml
 kubectl apply -f api-deployment.yaml
 kubectl apply -f worker-deployment.yaml
-kubectl apply -f redis-deployment.yaml
+kubectl apply -f redis-pod.yaml
 kubectl apply -f api-service.yaml
 kubectl apply -f redis-service.yaml
